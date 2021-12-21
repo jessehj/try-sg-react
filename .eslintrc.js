28111cfa12aed37bfd39206b55397756b7860393
jessehj <jessehj@slogup.com>
2021-12-21 11:24:34 +0900

    feat: Configure project with eslint & prettier

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "airbnb",
+    "airbnb-typescript",
+    "plugin:prettier/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.json",
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 13,
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint", "prettier"],
+  rules: {
+    "prettier/prettier": "error",
+    "@typescript-eslint/no-use-before-define": [
+      "error",
+      { functions: true, classes: true, variables: false },
+    ],
+    "react/require-default-props": "off",
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
